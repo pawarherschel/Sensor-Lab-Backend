d05b97590cc2932a0bffe3d1ee627571f371c20f
SakuraKat <SakuraKat1F338@gmail.com>
2023-03-25 23:27:40 +0530

    added templating and used it to make clone of index.html

---
+++ src/lib.rs
@@ -0,0 +1,62 @@
+use std::collections::HashMap;
+
+pub fn format_row(key: String, value: String, max: &u32) -> String {
+    let max = *max;
+    let height: f64 = value.parse::<u32>().unwrap() as f64 / max as f64;
+    let row_template = r#"<tr>
+            <th scope="row"><span class="drac-text drac-text-cyan"> <!--  {row_key}--></span></th>
+            <td style="--size:<!--  {row_height}-->;"> <span class="data"> <span
+                    class="drac-text drac-text-cyan"><!--  {row_data}--></span> </span> <span class="tooltip">data: <!--  {row_data}--></span>
+            </td>
+            <!-- {next_data}-->
+        </tr>"#;
+
+    row_template
+        .replace(r"<!--  {row_key}-->", key.as_str())
+        .replace(r"<!--  {row_data}-->", value.as_str())
+        .replace(r"<!--  {row_height}-->", height.to_string().as_str())
+        .replace(r"<!-- {next_data}-->", r#"<!--  {data}-->"#)
+}
+
+pub fn table_start() -> String {
+    r#"<table id="animated-bar" class="charts-css column show-labels show-data-on-hover show-primary-axis" style="height:200px;">
+        <thead>
+            <tr>
+                <th>data</th>
+            </tr>
+        </thead>
+        <tbody>
+        <!--  {data}-->"#
+        .to_string()
+}
+
+pub fn table_end() -> String {
+    r#"</tbody>
+    </table>"#
+        .to_string()
+}
+
+pub fn div_start(heading: String) -> String {
+    r#"<div class="drac-border-cyan" style="border-style: solid; border-radius: 25px; border-width: 1px;">
+    <div class="drac-box drac-p-md">
+    <h2 class="drac-heading drac-heading-xl drac-text-cyan">
+      <!--  {heading}-->
+    </h2>
+    <!--  {data}-->
+    "#.replace(r"<!--  {heading}-->", heading.as_str())
+}
+
+pub fn sort_data(data: &HashMap<String, (u32, u32)>) -> Vec<(String, u32, u32)> {
+    let mut data = data.clone();
+    let mut mut_data = {
+        let mut ret = Vec::new();
+        for (key, (order, value)) in data.iter_mut() {
+            ret.push((key.clone(), order.clone(), value.clone()));
+        }
+        ret
+    };
+
+    mut_data.sort_by(|(_, order1, _), (_, order2, _)| order1.cmp(order2));
+
+    mut_data.clone()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::fs::NamedFile;
-use rocket::response::content::RawHtml;
+use std::collections::HashMap;
 use std::env::current_dir;
 use std::path::PathBuf;
+
+use rocket::fs::NamedFile;
+use rocket::response::content::RawHtml;
+
+use sensor_lab_backend::{div_start, format_row, sort_data, table_end, table_start};
 
 #[get("/<file..>")]
 async fn files(file: PathBuf) -> Option<NamedFile> {
@@ -14,13 +18,47 @@ async fn files(file: PathBuf) -> Option<NamedFile> {
 }
 
 #[get("/")]
-async fn project<'a>() -> RawHtml<&'a str> {
+fn project<'a>() -> RawHtml<&'a str> {
     RawHtml(include_str!("../static/index.html"))
+}
+
+async fn poll_python() -> HashMap<String, (u32, u32)> {
+    let mut data = HashMap::new();
+    data.insert("test1".to_string(), (1, 1));
+    data.insert("test2".to_string(), (2, 2));
+    data.insert("test3".to_string(), (3, 3));
+    data
+}
+
+#[get("/")]
+async fn api() -> RawHtml<String> {
+    let data = poll_python().await;
+    let mut template = include_str!("../static/template.html").to_string();
+    template = template.replace(
+        r"<!--  {data}-->",
+        div_start("heading".to_string()).as_str(),
+    );
+    template = template.replace(r"<!--  {data}-->", table_start().as_str());
+    let values = data
+        .values()
+        .map(|(_order, value)| *value)
+        .collect::<Vec<u32>>();
+    let max = values.iter().max().unwrap();
+    let data = sort_data(&data);
+    for (key, _order, value) in data {
+        let value = format!("{}", value);
+        template = template.replace(
+            r"<!--  {data}-->",
+            format_row(key.clone(), value, max).as_str(),
+        );
+    }
+    template = template.replace(r"<!--  {next_data}-->", table_end().as_str());
+    RawHtml(template)
 }
 
 #[launch]
 fn rocket() -> rocket::Rocket<rocket::Build> {
     rocket::build()
         .mount("/", routes![files])
-        .mount("/project", routes![project])
+        .mount("/project", routes![api])
 }
